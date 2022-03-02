179a0b2995a99b721f5992072ee08e4aac7bd084
DarrylWitcher <Breach097@gmail.com>
2022-03-01 21:36:41 -0800

    Have the basic design the way I want

--- src/main/java/com/revature/pokedex/Pokemon.java
+++ src/main/java/com/revature/pokedex/Pokemon.java
@@ -32,12 +32,7 @@ import java.util.*;
 
     @Override
     public String toString() {
-        return "<HTML>\n" +
-                "<body>\n" + "<h1>"+ "<img src = \"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vi/omegaruby-alphasapphire/" + id +".png\" alt =pokemon>" + "\n"
-                + id + "\n" + name + "\n" + type1 + "," + type2 + "\n" + hp + "\n" + spd + "\n" + species + "\n"
-                + ht + "\n"  + wt + "\n"  +spAtk + "\n" +spDef+ "</h1>"
-                + "</body>" +
-                "</HTML>";
+        return display();
     }
 
     public int getId() {
@@ -289,10 +284,31 @@ import java.util.*;
     }
 
      public String display() {
-         return "<HTML>\n" +
-                 "<body>\n" + "<h1>"+ "<img src = \"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/" + id +".png\" alt =pokemon>" + "\n"
-                 + id + "\n" + name + "\n" + type1 + type2 + "</h1>"
-                 + "</body>" +
-                 "</HTML>";
+         return "<!DOCTYPE html>"+
+"<html lang=\"en\">"+
+"<head>"+
+    "<!-- Required meta tags -->"+
+    "<meta charset=\"utf-8\">"+
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"+
+
+    "<!-- Bootstrap CSS -->"+
+"<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">"
++ "<!-- Bootstrap Bundle with Popper --> "
+            + "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p\" crossorigin=\"anonymous\"></script>" +
+    "<title>Pokemon You Searched</title>"+
+"</head>"+
+"<body>"+
+
+"<div class=\"card\" style=\"width: 18rem;\">"+
+    "<img src= \"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-viii/icons/" + id + ".png\" class=\"card-img-top\" alt=\"pokemon\">" +
+    "<div class=\"card-body\">"+
+        "<h5 class=\"card-title\" id=\"name\">This is where the name should go</h5>"+
+        "<p class=\"card-text\">This is where dexId, name, and types are supposed to be</p>"+
+        "<a href=\"pokemon\" class=\"btn btn-primary\">View Pokemon</a>"+
+    "</div>"+
+"</div>"+
+"</body>"+
+"</html>";
+
      }
 }
